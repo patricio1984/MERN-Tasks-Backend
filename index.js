44f6707fb9a55887e33e70a086717319485296ee
patricio1984 <patricio1984@gmail.com>
2021-03-31 21:39:13 -0300

    Corrección en app.js tema Cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 conectarDB();
 
 //habilitar cors
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 
 //Habilitar express.json
 app.use(express.json({ extended: true }));
